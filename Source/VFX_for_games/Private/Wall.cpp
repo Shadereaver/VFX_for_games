e7ef915cb140f279fe19146dfd215c305e67d652
Shadereaver <117951478+Shadereaver@users.noreply.github.com>
2024-11-15 14:47:20 +0000

    Portal and wall effect

---
+++ Source/VFX_for_games/Private/Wall.cpp
@@ -0,0 +1,17 @@
+﻿#include "Wall.h"
+
+#include "Components/BoxComponent.h"
+
+AWall::AWall()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	_Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("_Collider"));
+	_Collider->SetBoxExtent(FVector(200, 10, 125));
+	RootComponent = _Collider;
+}
+
+void AWall::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/VFX_for_games/Private/Wall.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Wall.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class VFX_FOR_GAMES_API AWall : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWall();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UBoxComponent> _Collider;
+};
